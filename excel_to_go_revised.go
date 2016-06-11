2d0712eb1e2956d8f0922e5acf87e5c9890eff8c
LynneXie1201 <lynnexie1201@gmail.com>
2016-06-11 15:54:34 -0400

    Create excel_to_go_revised.go

---
+++ excel_to_go_revised.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tealeg/xlsx"
+)
+
+func main() {
+
+	excelFileName := "test_excel.xlsx"
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	var fileSlice [][][]string
+	fileSlice, _ = xlsx.FileToSlice("test_excel.xlsx") // Create a file slice
+
+	//col := xlFile.Sheets.MaxCol()  // get the colume number
+	keys := []string{}
+	for k := 0; k < 3; k++ {
+		keys = append(keys, fileSlice[0][0][k])
+
+	}
+
+	maps := make(map[int]map[string]string)
+
+	for _, sheet := range xlFile.Sheets {
+		for i, row := range sheet.Rows {
+			m := map[string]string{}
+			for j, cell := range row.Cells {
+				m[keys[j]] = cell.Value
+				fmt.Print(cell.Value + " ")
+
+			}
+			fmt.Printf("\n")
+			maps[i] = m
+		}
+		fmt.Printf("\n")
+	}
+	delete(maps, 0) // delete the header line
+	fmt.Println(maps)
+
+}
