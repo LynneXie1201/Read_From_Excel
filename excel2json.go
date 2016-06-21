ef95d6e2c37e28596dab4322d72ecdb9983aa165
Lynne Xie <Lynne Xie@sickkids.ca>
2016-06-21 10:36:15 -0400

    add error log messages

--- excel2json.go
+++ excel2json.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -12,7 +14,7 @@ import (
 	"github.com/tealeg/xlsx"
 )
 
-// global variables, to store the corresponding events
+// global variables
 var (
 	allFollowUps []followUp
 	allLKA       []lkaDate
@@ -21,6 +23,9 @@ var (
 	allTE        []te
 	allSBE       []sbe
 	events       []general //events including FUMI, FUPACE, SVD, PVL, DVT, ARH, THRM, HEML
+	e            *log.Logger
+	codes        []string
+	nums         []int
 )
 
 // Create types of events
@@ -61,6 +66,19 @@ type general struct {
 
 // Helper functions
 
+// Generate error messages to file and terminal
+func newError(path string, id string, row int, t string, field string, invalid string) {
+	errLog, err := os.OpenFile("errlog.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	multi := io.MultiWriter(errLog, os.Stdout)
+	e = log.New(multi,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	e.Println("File Path:", path, "PTID:", id, "Row #:", row+2, "Type:", t, "Info: Invalid", field, "Value:", invalid)
+}
+
 // Change the date Format to YYYY-MM-DD
 func changeDateFormat(x string) string {
 	value := strings.Replace(x, "\\", "", -1)
@@ -91,6 +109,15 @@ func stringInSlice(str string, list []string) bool {
 	return false
 }
 
+func intInSlice(i int, list []int) bool {
+	for _, v := range list {
+		if v == i {
+			return true
+		}
+	}
+	return false
+}
+
 // Check if the excel file is a follow_up file and return the header row
 func checkFollowups(excelFileName string) (bool, []string) {
 	xlFile, err := xlsx.OpenFile(excelFileName)
@@ -111,7 +138,7 @@ func checkFollowups(excelFileName string) (bool, []string) {
 	return false, nil
 }
 
-// for loop all excel files in a folder
+// Recursively for loop all files in the folder
 func loopAllFiles(dirPath string) {
 	fileList := []string{}
 	err := filepath.Walk(dirPath, func(path string, f os.FileInfo, err error) error {
@@ -134,6 +161,7 @@ func excelToSlice(excelFileName string) []map[string]string {
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
+
 	value, keys := checkFollowups(excelFileName)
 	if value == true {
 		slices := []map[string]string{} // each row is a map
@@ -161,8 +189,9 @@ func readExcelData(path string) {
 	if s == nil {
 		fmt.Println("no! this is not a follow_up file: ", path)
 	} else {
-		for _, m := range s {
-			// Event follow_ups
+		fmt.Println("yes! this is a follow_up file: ", path)
+		for i, m := range s {
+			// Event follow_up
 			if m["FU_D"] != "" {
 				fU := followUp{
 					PTID:          m["PTID"],
@@ -177,6 +206,19 @@ func readExcelData(path string) {
 				fU.Coag, _ = strconv.Atoi(m["COAG"])
 				fU.PoNYHA, _ = strconv.Atoi(m["PO_NYHA"])
 				fU.Plat, _ = strconv.Atoi(m["PLAT"])
+				// Validate fields' values
+				if !stringInSlice(fU.Status, codes) {
+					newError(path, fU.PTID, i, fU.Type, "Status", fU.Status)
+				}
+				if !intInSlice(fU.PoNYHA, nums[1:5]) {
+					newError(path, fU.PTID, i, fU.Type, "PO_NYHA", m["PO_NYHA"])
+				}
+				if !intInSlice(fU.Coag, nums[:2]) {
+					newError(path, fU.PTID, i, fU.Type, "COAG", m["COAG"])
+				}
+				if !intInSlice(fU.Plat, nums[0:2]) {
+					newError(path, fU.PTID, i, fU.Type, "PLAT", m["PLAT"])
+				}
 				allFollowUps = append(allFollowUps, fU)
 			}
 			// Event LAST KNOWN ALIVE DATE
@@ -188,7 +230,7 @@ func readExcelData(path string) {
 				allLKA = append(allLKA, l)
 			}
 			// Event Death
-			if m["DIED"] == "1" {
+			if m["DTH_D"] != "" {
 				d := death{
 					PTID:   m["PTID"],
 					Type:   "death",
@@ -196,11 +238,18 @@ func readExcelData(path string) {
 					Reason: m["REASDTH"]}
 				d.PrmDth, _ = strconv.Atoi(m["PRM_DTH"])
 				d.Code, _ = strconv.Atoi(m["DIED"])
+				// Validate fields' values
+				if !intInSlice(d.Code, nums[:2]) {
+					newError(path, d.PTID, i, d.Type, "DIED", m["DIED"])
+				}
+				if !intInSlice(d.PrmDth, nums[:5]) {
+					newError(path, d.PTID, i, d.Type, "PRM_DTH", m["PRM_DTH"])
+				}
 				allDths = append(allDths, d)
 			}
 
 			// Event FUREOP
-			if m["FUREOP"] == "1" {
+			if m["FUREOP_D"] != "" {
 				re := reOperation{
 					PTID:    m["PTID"],
 					Type:    "FUREOP",
@@ -210,6 +259,10 @@ func readExcelData(path string) {
 					Notes:   m["REOPNOTES"]}
 				re.Code, _ = strconv.Atoi(m["FUREOP"])
 				re.Survival, _ = strconv.Atoi(m["REOPSURVIVAL"])
+				// Validate fields' values
+				if !intInSlice(re.Code, nums[:2]) {
+					newError(path, re.PTID, i, re.Type, "FUREOP", m["FUREOP"])
+				}
 				allReOper = append(allReOper, re)
 			}
 
@@ -222,6 +275,15 @@ func readExcelData(path string) {
 				te1.Code, _ = strconv.Atoi(m["TE1"])
 				te1.Outcome, _ = strconv.Atoi(m["TE1_OUT"])
 				te1.Anti, _ = strconv.Atoi(m["ANTI_TE1"])
+				if !intInSlice(te1.Code, nums[:4]) {
+					newError(path, te1.PTID, i, te1.Type, "TE1", m["TE1"])
+				}
+				if !intInSlice(te1.Outcome, nums[:4]) {
+					newError(path, te1.PTID, i, te1.Type, "TE1_OUT", m["TE1_OUT"])
+				}
+				if !intInSlice(te1.Anti, nums[:4]) && (te1.Anti != 8) {
+					newError(path, te1.PTID, i, te1.Type, "ANTI_TE1", m["ANTI_TE1"])
+				}
 				allTE = append(allTE, te1)
 			}
 			if m["TE2_D"] != "" {
@@ -232,6 +294,15 @@ func readExcelData(path string) {
 				te2.Code, _ = strconv.Atoi(m["TE2"])
 				te2.Outcome, _ = strconv.Atoi(m["TE2_OUT"])
 				te2.Anti, _ = strconv.Atoi(m["ANTI_TE2"])
+				if !intInSlice(te2.Code, nums[:4]) {
+					newError(path, te2.PTID, i, te2.Type, "TE2", m["TE2"])
+				}
+				if !intInSlice(te2.Outcome, nums[:4]) {
+					newError(path, te2.PTID, i, te2.Type, "TE2_OUT", m["TE2_OUT"])
+				}
+				if !intInSlice(te2.Anti, nums[:4]) && (te2.Anti != 8) {
+					newError(path, te2.PTID, i, te2.Type, "ANTI_TE2", m["ANTI_TE2"])
+				}
 				allTE = append(allTE, te2)
 			}
 			if m["TE3_D"] != "" {
@@ -242,6 +313,15 @@ func readExcelData(path string) {
 				te3.Code, _ = strconv.Atoi(m["TE3"])
 				te3.Outcome, _ = strconv.Atoi(m["TE3_OUT"])
 				te3.Anti, _ = strconv.Atoi(m["ANTI_TE3"])
+				if !intInSlice(te3.Code, nums[:4]) {
+					newError(path, te3.PTID, i, te3.Type, "TE3", m["TE3"])
+				}
+				if !intInSlice(te3.Outcome, nums[:4]) {
+					newError(path, te3.PTID, i, te3.Type, "TE3_OUT", m["TE3_OUT"])
+				}
+				if !intInSlice(te3.Anti, nums[:4]) && (te3.Anti != 8) {
+					newError(path, te3.PTID, i, te3.Type, "ANTI_TE3", m["ANTI_TE3"])
+				}
 				allTE = append(allTE, te3)
 			}
 
@@ -268,25 +348,40 @@ func readExcelData(path string) {
 			// Event SBE
 			if m["SBE1_D"] != "" {
 				sbe1 := sbe{
-					Type: "SBE",
-					Date: m["SBE1_D"]}
+					PTID:     m["PTID"],
+					Type:     "SBE",
+					Date:     m["SBE1_D"],
+					Organism: m["SBE1 ORGANISM"]}
 				sbe1.Code, _ = strconv.Atoi(m["SBE1"])
+				if !intInSlice(sbe1.Code, nums[:2]) {
+					newError(path, sbe1.PTID, i, sbe1.Type, "SBE1", m["SBE1"])
+				}
 				allSBE = append(allSBE, sbe1)
 			}
 
 			if m["SBE2_D"] != "" {
 				sbe2 := sbe{
-					Type: "SBE",
-					Date: m["SBE2_D"]}
+					PTID:     m["PTID"],
+					Type:     "SBE",
+					Date:     m["SBE2_D"],
+					Organism: m["SBE2 ORGANISM"]}
 				sbe2.Code, _ = strconv.Atoi(m["SBE2"])
+				if !intInSlice(sbe2.Code, nums[:2]) {
+					newError(path, sbe2.PTID, i, sbe2.Type, "SBE2", m["SBE2"])
+				}
 				allSBE = append(allSBE, sbe2)
 			}
 
 			if m["SBE3_D"] != "" {
 				sbe3 := sbe{
-					Type: "SBE",
-					Date: m["SBE3_D"]}
+					PTID:     m["PTID"],
+					Type:     "SBE",
+					Date:     m["SBE3_D"],
+					Organism: m["SBE3 ORGANISM"]}
 				sbe3.Code, _ = strconv.Atoi(m["SBE3"])
+				if !intInSlice(sbe3.Code, nums[:2]) {
+					newError(path, sbe3.PTID, i, sbe3.Type, "SBE3", m["SBE3"])
+				}
 				allSBE = append(allSBE, sbe3)
 			}
 
@@ -334,6 +429,9 @@ func readExcelData(path string) {
 					Type: "ARH",
 					Date: m["ARH1_D"]}
 				arh1.Code, _ = strconv.Atoi(m["ARH1"])
+				if !intInSlice(arh1.Code, nums[:]) {
+					newError(path, arh1.PTID, i, arh1.Type, "ARH1", m["ARH1"])
+				}
 				events = append(events, arh1)
 			}
 
@@ -343,6 +441,9 @@ func readExcelData(path string) {
 					Type: "ARH",
 					Date: m["ARH2_D"]}
 				arh2.Code, _ = strconv.Atoi(m["ARH2"])
+				if !intInSlice(arh2.Code, nums[:]) {
+					newError(path, arh2.PTID, i, arh2.Type, "ARH2", m["ARH2"])
+				}
 				events = append(events, arh2)
 			}
 
@@ -384,16 +485,13 @@ func readExcelData(path string) {
 				events = append(events, heml2)
 			}
 		}
-		fmt.Println("yes! this is a follow_up file: ", path)
 	}
 }
 
 func main() {
-<<<<<<< HEAD
+	nums = []int{0, 1, 2, 3, 4, 5}
+	codes = []string{"A", "D", "L", "N", "O", "R"}
 	loopAllFiles("L:/CVDMC Students/Yilin Xie/data/excel")
-	fmt.Println(events)
-=======
-	loopAllFiles(your_folder_path)
-	//fmt.Println(allDths)
->>>>>>> fd087400610863f13b7f607ca8e03aba35f3935d
+	fmt.Println(allSBE)
+
 }
