104501b1c469f257557e4b9a3f815ece9aa53f32
LynneXie1201 <lynnexie1201@gmail.com>
2016-07-06 16:43:48 -0400

    Update excel2json.go

--- excel2json.go
+++ excel2json.go
@@ -21,10 +21,7 @@ var (
 	events       []general     //store events including FUMI, FUPACE, SVD, PVL, DVT, ARH, THRM, HEML
 	codes        []string      // status codes
 	nums         []int         // numerical values for various codes
-	id1          string        // value of the first PTID column
-	id2          string        // value of the second PTID column
-	s1           string        // value of the first Status column
-	s2           string        // value of the second Status column
+
 )
 
 // Create different ypes of events
@@ -66,12 +63,11 @@ type general struct {
 func init() {
 	nums = []int{0, -9, 1, 2, 3, 4, 5}                 // list of numbers for validate codes
 	codes = []string{"A", "D", "L", "N", "O", "R", ""} // correct codes for STATUS
-
 }
 
 // CheckPtidColumns checks the number of PTID columns,
 // assume each file would have at most two PTID columns.
-func CheckPtidColumns(e *log.Logger, path string, j int, keys []string) {
+func CheckPtidColumns(e *log.Logger, path string, j int, keys []string) (string, string) {
 	id := []string{}
 	for _, k := range keys {
 		if strings.Contains(k, "PTID") {
@@ -79,18 +75,23 @@ func CheckPtidColumns(e *log.Logger, path string, j int, keys []string) {
 		}
 	}
 	if len(id) == 2 {
-		id1, id2 = id[0], id[1]
+		id1, id2 := id[0], id[1]
+		return id1, id2
 	} else if len(id) == 1 {
-		id1, id2 = id[0], id[0]
-	} else {
-		errlog.Invalid(e, 0, path, j)
-		os.Exit(1) // exit if it has invaid columns of PTID
+		id1, id2 := id[0], id[0]
+		return id1, id2
 	}
+
+	errlog.Invalid(e, 0, path, j)
+
+	os.Exit(1) // exit if it has invaid columns of PTID
+	return "", ""
+
 }
 
 // CheckStatusColumns checks the number of STATUS columns,
 // assume each file would have at most two STATUS columns.
-func CheckStatusColumns(e *log.Logger, path string, j int, keys []string) {
+func CheckStatusColumns(e *log.Logger, path string, j int, keys []string) (string, string) {
 	status := []string{}
 	for _, k := range keys {
 		matched, err := regexp.MatchString("^.*STATUS$", k) // check status's pattern
@@ -100,13 +101,15 @@ func CheckStatusColumns(e *log.Logger, path string, j int, keys []string) {
 		}
 	}
 	if len(status) == 2 {
-		s1, s2 = status[0], status[1]
+		s1, s2 := status[0], status[1]
+		return s1, s2
 	} else if len(status) == 1 {
-		s1, s2 = status[0], status[0]
-	} else {
-		errlog.Invalid(e, 1, path, j)
-		os.Exit(1)
+		s1, s2 := status[0], status[0]
+		return s1, s2
 	}
+	errlog.Invalid(e, 1, path, j)
+	os.Exit(1)
+	return "", ""
 }
 
 // LoopAllFiles recursively loops all files in a folder, and tracks all excel files,
@@ -144,43 +147,47 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 			fmt.Println("Bingo! this is a follow_up sheet: ", path, "sheet #:", j)
 			keys := keyList[j]
 			// check PTID and STATUS
-			CheckPtidColumns(e, path, j, keys)
-			CheckStatusColumns(e, path, j, keys)
+			p1, p2 := CheckPtidColumns(e, path, j, keys)
+			st1, st2 := CheckStatusColumns(e, path, j, keys)
 			// i is the index of rows
 			for i, m := range s {
 				// check PTID
-				if m[id1] != m[id2] {
-					helper.AssignPTID(e, path, i, j, m[id1], m[id2])
+				ID1, ID2 := m[p1], m[p2]
+				if ID1 != ID2 {
+					helper.AssignPTID(e, path, i, j, &ID1, &ID2)
+
 				}
-				if len(m[id1]) != 10 {
-					errlog.Differ(e, 2, path, j, i, m[id1], "")
+				if len(ID1) != 10 {
+					errlog.Differ(e, 2, path, j, i, ID1, "")
 				} else {
-					matched, err := regexp.MatchString("(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01])", m[id1][4:8]) // validate MM and DD part of a PTID
+					matched, err := regexp.MatchString("(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01])", ID1[4:8]) // validate MM and DD part of a PTID
 					helper.CheckErr(e, err)
 					if !matched {
-						errlog.Differ(e, 3, path, j, i, m[id1], "")
+						errlog.Differ(e, 3, path, j, i, ID1, "")
 					}
 				}
 				// Check STATUS
-				if m[s1] != m[s2] {
-					helper.AssignStatus(e, path, i, j, m[s1], m[s2])
+				S1, S2 := m[st1], m[st2]
+				if S1 != S2 {
+					helper.AssignStatus(e, path, i, j, &S1, &S2)
+
 				}
 				// Event follow_up
 				if m["FU_D"] != "" {
 					fU := followUp{
-						PTID:          m[id1],
+						PTID:          ID1,
 						Date:          helper.CheckDateFormat(e, path, j, i, "follow_up Date", m["FU_D"]),
 						Type:          "followup",
-						Status:        m[s1],
+						Status:        S1,
 						NoneValveReop: m["NONVALVE REOP"],
 						FuNotes:       m["FU NOTES"],
 						Notes:         m["NOTES"],
 						LostOnDate:    m["STATUS=L DATE"],
 						OtherNote:     m["STATUS=O REASON"]}
 					// check if these 3 columns are empty or not
-					helper.CheckEmpty(fU.Coag, m["COAG"], e)
-					helper.CheckEmpty(fU.PoNYHA, m["PO_NYHA"], e)
-					helper.CheckEmpty(fU.Plat, m["PLAT"], e)
+					helper.CheckEmpty(&fU.Coag, m["COAG"])
+					helper.CheckEmpty(&fU.PoNYHA, m["PO_NYHA"])
+					helper.CheckEmpty(&fU.Plat, m["PLAT"])
 
 					// Validate fields' values
 					if !helper.StringInSlice(fU.Status, codes) {
@@ -201,7 +208,7 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event LAST KNOWN ALIVE DATE
 				if m["LKA_D"] != "" {
 					l := lkaDate{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "LKA_D",
 						Date: helper.CheckDateFormat(e, path, j, i, "LKA_Date", m["LKA_D"])}
 					helper.WriteTOFile(jsonFile, l)
@@ -210,13 +217,13 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event Death
 				if m["DTH_D"] != "" {
 					d := death{
-						PTID:   m[id1],
+						PTID:   ID1,
 						Type:   "death",
 						Date:   helper.CheckDateFormat(e, path, j, i, "DTH_Date", m["DTH_D"]),
 						Reason: m["REASDTH"]}
 
-					helper.CheckEmpty(d.PrmDth, m["PRM_DTH"], e)
-					helper.CheckEmpty(d.Code, m["DIED"], e)
+					helper.CheckEmpty(&d.PrmDth, m["PRM_DTH"])
+					helper.CheckEmpty(&d.Code, m["DIED"])
 
 					// Validate fields' values
 					if !helper.IntInSlice(d.Code, nums[:3]) {
@@ -232,21 +239,21 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event FUREOP
 				if m["FUREOP_D"] != "" {
 					re := reOperation{
-						PTID:    m[id1],
+						PTID:    ID1,
 						Type:    "FUREOP",
 						Date:    helper.CheckDateFormat(e, path, j, i, "FUREOP_Date", m["FUREOP_D"]),
 						Reason:  m["REASREOP"],
 						Surgery: m["REOPSURG"],
 						Notes:   m["REOPNOTES"]}
-					helper.CheckEmpty(re.Code, m["FUREOP"], e)
-					helper.CheckEmpty(re.Survival, m["REOPSURVIVAL"], e)
+					helper.CheckEmpty(&re.Code, m["FUREOP"])
+					helper.CheckEmpty(&re.Survival, m["REOPSURVIVAL"])
 
 					// Validate fields' values
 					if !helper.IntInSlice(re.Code, nums[:3]) {
 						errlog.ErrorLog(e, path, j, re.PTID, i, re.Type, "FUREOP", m["FUREOP"])
 					}
-					if m[s1] == "R" && (re.Code != 1 || m["FUREOP_D"] == "") {
-						errlog.Differ(e, 4, path, j, i, m[id1], "")
+					if S1 == "R" && (re.Code != 1 || m["FUREOP_D"] == "") {
+						errlog.Differ(e, 4, path, j, i, re.PTID, "")
 					}
 					helper.WriteTOFile(jsonFile, re)
 					allReOper = append(allReOper, re)
@@ -255,13 +262,13 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event TE
 				if m["TE1_D"] != "" {
 					te1 := te{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "TE",
 						Date: helper.CheckDateFormat(e, path, j, i, "TE1_Date", m["TE1_D"])}
 
-					helper.CheckEmpty(te1.Code, m["TE1"], e)
-					helper.CheckEmpty(te1.Outcome, m["TE1_OUT"], e)
-					helper.CheckEmpty(te1.Anti, m["ANTI_TE1"], e)
+					helper.CheckEmpty(&te1.Code, m["TE1"])
+					helper.CheckEmpty(&te1.Outcome, m["TE1_OUT"])
+					helper.CheckEmpty(&te1.Anti, m["ANTI_TE1"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(te1.Code, nums[:5]) {
@@ -278,13 +285,13 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				}
 				if m["TE2_D"] != "" {
 					te2 := te{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "TE",
 						Date: helper.CheckDateFormat(e, path, j, i, "TE2_Date", m["TE2_D"])}
 
-					helper.CheckEmpty(te2.Code, m["TE2"], e)
-					helper.CheckEmpty(te2.Outcome, m["TE2_OUT"], e)
-					helper.CheckEmpty(te2.Anti, m["ANTI_TE2"], e)
+					helper.CheckEmpty(&te2.Code, m["TE2"])
+					helper.CheckEmpty(&te2.Outcome, m["TE2_OUT"])
+					helper.CheckEmpty(&te2.Anti, m["ANTI_TE2"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(te2.Code, nums[:5]) {
@@ -301,13 +308,13 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				}
 				if m["TE3_D"] != "" {
 					te3 := te{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "TE",
 						Date: helper.CheckDateFormat(e, path, j, i, "TE3_Date", m["TE3_D"])}
 
-					helper.CheckEmpty(te3.Code, m["TE3"], e)
-					helper.CheckEmpty(te3.Outcome, m["TE3_OUT"], e)
-					helper.CheckEmpty(te3.Anti, m["ANTI_TE3"], e)
+					helper.CheckEmpty(&te3.Code, m["TE3"])
+					helper.CheckEmpty(&te3.Outcome, m["TE3_OUT"])
+					helper.CheckEmpty(&te3.Anti, m["ANTI_TE3"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(te3.Code, nums[:5]) {
@@ -326,11 +333,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event FUMI
 				if m["FUMI_D"] != "" {
 					f1 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "FUMI",
 						Date: helper.CheckDateFormat(e, path, j, i, "FUMI_Date", m["FUMI_D"])}
 
-					helper.CheckEmpty(f1.Code, m["FUMI"], e)
+					helper.CheckEmpty(&f1.Code, m["FUMI"])
 
 					helper.WriteTOFile(jsonFile, f1)
 					events = append(events, f1)
@@ -339,11 +346,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event FUPACE
 				if m["FUPACE_D"] != "" {
 					f2 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "FUPACE",
 						Date: helper.CheckDateFormat(e, path, j, i, "FUPACE_Date", m["FUPACE_D"])}
 
-					helper.CheckEmpty(f2.Code, m["FUPACE"], e)
+					helper.CheckEmpty(&f2.Code, m["FUPACE"])
 
 					helper.WriteTOFile(jsonFile, f2)
 					events = append(events, f2)
@@ -352,11 +359,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event SBE
 				if m["SBE1_D"] != "" {
 					sbe1 := sbe{
-						PTID:     m[id1],
+						PTID:     ID1,
 						Type:     "SBE",
 						Date:     helper.CheckDateFormat(e, path, j, i, "SBE1_Date", m["SBE1_D"]),
 						Organism: m["SBE1 ORGANISM"]}
-					helper.CheckEmpty(sbe1.Code, m["SBE1"], e)
+					helper.CheckEmpty(&sbe1.Code, m["SBE1"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(sbe1.Code, nums[:3]) {
@@ -368,11 +375,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 
 				if m["SBE2_D"] != "" {
 					sbe2 := sbe{
-						PTID:     m[id1],
+						PTID:     ID1,
 						Type:     "SBE",
 						Date:     helper.CheckDateFormat(e, path, j, i, "SBE2_Date", m["SBE2_D"]),
 						Organism: m["SBE2 ORGANISM"]}
-					helper.CheckEmpty(sbe2.Code, m["SBE2"], e)
+					helper.CheckEmpty(&sbe2.Code, m["SBE2"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(sbe2.Code, nums[:3]) {
@@ -384,12 +391,12 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 
 				if m["SBE3_D"] != "" {
 					sbe3 := sbe{
-						PTID:     m[id1],
+						PTID:     ID1,
 						Type:     "SBE",
 						Date:     helper.CheckDateFormat(e, path, j, i, "SBE3_Date", m["SBE3_D"]),
 						Organism: m["SBE3 ORGANISM"]}
 
-					helper.CheckEmpty(sbe3.Code, m["SBE3"], e)
+					helper.CheckEmpty(&sbe3.Code, m["SBE3"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(sbe3.Code, nums[:3]) {
@@ -402,11 +409,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event SVD
 				if m["SVD_D"] != "" {
 					s4 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "SVD",
 						Date: helper.CheckDateFormat(e, path, j, i, "SVD_Date", m["SVD_D"])}
 
-					helper.CheckEmpty(s4.Code, m["SVD"], e)
+					helper.CheckEmpty(&s4.Code, m["SVD"])
 
 					helper.WriteTOFile(jsonFile, s4)
 					events = append(events, s4)
@@ -414,11 +421,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event PVL
 				if m["PVL1_D"] != "" {
 					pvl1 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "PVL",
 						Date: helper.CheckDateFormat(e, path, j, i, "PVL1_Date", m["PVL1_D"])}
 
-					helper.CheckEmpty(pvl1.Code, m["PVL1"], e)
+					helper.CheckEmpty(&pvl1.Code, m["PVL1"])
 
 					helper.WriteTOFile(jsonFile, pvl1)
 					events = append(events, pvl1)
@@ -426,10 +433,10 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 
 				if m["PVL2_D"] != "" {
 					pvl2 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "PVL",
 						Date: helper.CheckDateFormat(e, path, j, i, "PVL2_Date", m["PVL2_D"])}
-					helper.CheckEmpty(pvl2.Code, m["PVL2"], e)
+					helper.CheckEmpty(&pvl2.Code, m["PVL2"])
 
 					helper.WriteTOFile(jsonFile, pvl2)
 					events = append(events, pvl2)
@@ -438,11 +445,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event DVT
 				if m["DVT_D"] != "" {
 					d1 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "DVT",
 						Date: helper.CheckDateFormat(e, path, j, i, "DVT_Date", m["DVT_D"])}
 
-					helper.CheckEmpty(d1.Code, m["DVT"], e)
+					helper.CheckEmpty(&d1.Code, m["DVT"])
 
 					helper.WriteTOFile(jsonFile, d1)
 					events = append(events, d1)
@@ -450,10 +457,10 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event ARH
 				if m["ARH1_D"] != "" {
 					arh1 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "ARH",
 						Date: helper.CheckDateFormat(e, path, j, i, "ARH1_Date", m["ARH1_D"])}
-					helper.CheckEmpty(arh1.Code, m["ARH1"], e)
+					helper.CheckEmpty(&arh1.Code, m["ARH1"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(arh1.Code, nums[:]) {
@@ -465,10 +472,10 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 
 				if m["ARH2_D"] != "" {
 					arh2 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "ARH",
 						Date: helper.CheckDateFormat(e, path, j, i, "ARH2_Date", m["ARH2_D"])}
-					helper.CheckEmpty(arh2.Code, m["ARH2"], e)
+					helper.CheckEmpty(&arh2.Code, m["ARH2"])
 
 					// Generate Error Messages
 					if !helper.IntInSlice(arh2.Code, nums[:]) {
@@ -481,11 +488,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event THRM
 				if m["THRM1_D"] != "" {
 					thrm1 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "THRM",
 						Date: helper.CheckDateFormat(e, path, j, i, "THRM1_Date", m["THRM1_D"])}
 
-					helper.CheckEmpty(thrm1.Code, m["THRM1"], e)
+					helper.CheckEmpty(&thrm1.Code, m["THRM1"])
 
 					helper.WriteTOFile(jsonFile, thrm1)
 					events = append(events, thrm1)
@@ -493,10 +500,10 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 
 				if m["THRM2_D"] != "" {
 					thrm2 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "THRM",
 						Date: helper.CheckDateFormat(e, path, j, i, "THRM2_Date", m["THRM2_D"])}
-					helper.CheckEmpty(thrm2.Code, m["THRM2"], e)
+					helper.CheckEmpty(&thrm2.Code, m["THRM2"])
 
 					helper.WriteTOFile(jsonFile, thrm2)
 					events = append(events, thrm2)
@@ -505,11 +512,11 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 				// Event HEML
 				if m["HEML1_D"] != "" {
 					heml1 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "HEML",
 						Date: helper.CheckDateFormat(e, path, j, i, "HEML1_Date", m["HEML1_D"])}
 
-					helper.CheckEmpty(heml1.Code, m["HEML1"], e)
+					helper.CheckEmpty(&heml1.Code, m["HEML1"])
 
 					helper.WriteTOFile(jsonFile, heml1)
 					events = append(events, heml1)
@@ -517,10 +524,10 @@ func ReadExcelData(e *log.Logger, path string, jsonFile *os.File) {
 
 				if m["HEML2_D"] != "" {
 					heml2 := general{
-						PTID: m[id1],
+						PTID: ID1,
 						Type: "HEML",
 						Date: helper.CheckDateFormat(e, path, j, i, "HEML2_Date", m["HEML2_D"])}
-					helper.CheckEmpty(heml2.Code, m["HEML2"], e)
+					helper.CheckEmpty(&heml2.Code, m["HEML2"])
 					helper.WriteTOFile(jsonFile, heml2)
 					events = append(events, heml2)
 				}
