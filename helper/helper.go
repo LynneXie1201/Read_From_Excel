4fae2dcbbc2c4474a8d4c23ac9c1a9a9b7837e5d
LynneXie1201 <lynnexie1201@gmail.com>
2016-07-12 17:00:06 -0400

    Update helper.go

--- helper/helper.go
+++ helper/helper.go
@@ -23,13 +23,17 @@ func CheckDateFormat(e *log.Logger, path string, sheet int, row int, column stri
 	value = strings.Replace(value, ";", "", -1)
 	value = strings.Replace(value, "@", "", -1)
 	// YYYY-MM-DD
-	matched1, _ := regexp.MatchString("^[0-9]{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$", value)
+	matched1, err := regexp.MatchString("^[0-9]{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$", value)
+	CheckErr(e, err)
 	// DD-MMM-YY
-	matched2, _ := regexp.MatchString("^(0?[1-9]|[12][0-9]|3[01])-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-[0-9]{2}$", value)
+	matched2, err := regexp.MatchString("^(0?[1-9]|[12][0-9]|3[01])-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-[0-9]{2}$", value)
+	CheckErr(e, err)
 	// MM-DD-YY
-	matched3, _ := regexp.MatchString("^(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])-[0-9]{2}$", value)
+	matched3, err := regexp.MatchString("^(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])-[0-9]{2}$", value)
+	CheckErr(e, err)
 	// YYYY/MM/DD
-	matched4, _ := regexp.MatchString("^[0-9]{4}/(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])$", value)
+	matched4, err := regexp.MatchString("^[0-9]{4}/(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])$", value)
+	CheckErr(e, err)
 	if matched1 {
 		return value
 	} else if matched2 {
@@ -50,9 +54,10 @@ func CheckDateFormat(e *log.Logger, path string, sheet int, row int, column stri
 			errlog.Differ(e, 5, path, sheet, row, column, value)
 		}
 		return t.Format("2006-01-02")
+	} else {
+		errlog.Differ(e, 5, path, sheet, row, column, value)
+		return value
 	}
-	errlog.Differ(e, 5, path, sheet, row, column, value)
-	return value
 }
 
 // StringInSlice checks if a slice contains a certain string value
@@ -78,7 +83,7 @@ func IntInSlice(i int, list []int) bool {
 // CheckErr checks errors, and prints error messages to error logs and screen
 func CheckErr(e *log.Logger, err error) {
 	if err != nil {
-		errlog.PrintErr(e, err)    // print to error log
+		e.Println(err)             // print to error log
 		log.Fatalln("ERROR:", err) // print to terminal and then terminate
 	}
 }
@@ -113,18 +118,25 @@ func AssignPTID(e *log.Logger, path string, i int, j int, d1 *string, d2 *string
 
 // CheckHeaderRow checks if the header rows cannot be read,
 // then use a package to open the file and save it.
-func CheckHeaderRow(xlFile *xlsx.File, path string) {
-
-	// Check if the header row is empty
-	v, _ := xlFile.Sheets[0].Cell(0, 0).String()
+func CheckHeaderRow(e *log.Logger, excelFilePath string) (bool, *xlsx.File) {
+	// Open an excel file
+	File, err := xlsx.OpenFile(excelFilePath)
+	CheckErr(e, err)
+	// assign the A1 cell to v
+	v, _ := File.Sheets[0].Cell(0, 0).String()
+	//CheckErr(e, err)
+	// Check if the header row is empty,
 	// if the header rows cannot be read, use a package to open the file and then save it.
 	if v == "" {
-		option := excel.Option{"Visible": true, "DisplayAlerts": true}
-		xl, _ := excel.Open(path, option)
+		option := excel.Option{"Visible": false, "DisplayAlerts": true}
+		xl, err := excel.Open(excelFilePath, option)
+		CheckErr(e, err)
 		defer xl.Quit()
 		xl.Save()
 		xl.Quit()
+		return true, nil
 	}
+	return false, File
 
 }
 
@@ -135,6 +147,7 @@ func CheckFollowups(e *log.Logger, path string, j int, sheet *xlsx.Sheet) (bool,
 
 	// Check if the header row is empty
 	v, _ := sheet.Cell(0, 0).String()
+	//CheckErr(e, err)
 	if v == "" {
 		errlog.Invalid(e, 2, path, j)
 		return false, nil
@@ -142,7 +155,8 @@ func CheckFollowups(e *log.Logger, path string, j int, sheet *xlsx.Sheet) (bool,
 	keys := []string{}
 	for _, row := range sheet.Rows {
 		for _, cell := range row.Cells {
-			value, _ := cell.String()
+			value := cell.Value
+			//CheckErr(e, err)
 			keys = append(keys, value)
 		}
 		break
@@ -163,13 +177,11 @@ func CheckFollowups(e *log.Logger, path string, j int, sheet *xlsx.Sheet) (bool,
 // and each sheet is restructed to a slice containing list of maps.
 func ExcelToSlice(e *log.Logger, excelFilePath string) ([][]map[string]string, [][]string) {
 
-	File, err := xlsx.OpenFile(excelFilePath)
-	CheckErr(e, err)
-
-	CheckHeaderRow(File, excelFilePath)
-
-	xlFile, err := xlsx.OpenFile(excelFilePath)
-	CheckErr(e, err)
+	isEmpty, xlFile := CheckHeaderRow(e, excelFilePath)
+	// if the excel file has a empty header row
+	if isEmpty {
+		xlFile, _ = xlsx.OpenFile(excelFilePath)
+	}
 
 	slices := [][]map[string]string{}
 	keyList := [][]string{}
@@ -182,6 +194,7 @@ func ExcelToSlice(e *log.Logger, excelFilePath string) ([][]map[string]string, [
 				m := map[string]string{} // a row is a map
 				for j, cell := range row.Cells {
 					value, _ := cell.String()
+					//CheckErr(e, err)
 					if value == "9" {
 						value = "-9"
 					}
@@ -224,4 +237,68 @@ func CheckEmpty(value1 *int, value2 string) {
 	} else {
 		*value1 = -9
 	}
+}
+
+// CheckPtidColumns checks the number of PTID columns,
+// and returns the column names of PTID, assuming each file would have at most two PTID columns.
+// Parameters including:
+// e - a logger that records the error messages;
+// path - the path of the excel file;
+// j - the index of the sheets in that excel file;
+// keys - a slice that contains the header row
+func CheckPtidColumns(e *log.Logger, path string, j int, keys []string) (string, string) {
+	// create a slice that holds the column names that contains PTID
+	id := []string{}
+	for _, k := range keys {
+		if strings.Contains(k, "PTID") {
+			id = append(id, k)
+		}
+	}
+	// if len is 2, we have 2 columns of PTID
+	if len(id) == 2 {
+		id1, id2 := id[0], id[1]
+		return id1, id2
+		// if len is 1, we have only one column of PTID
+	} else if len(id) == 1 {
+		id1, id2 := id[0], id[0]
+		return id1, id2
+	}
+	// else would be invaid as we assume each file would have at most two PTID columns,
+	// then an error message gets written and the program stops.
+	errlog.Invalid(e, 0, path, j)
+	os.Exit(1) // exit if it has invaid columns of PTID
+	return "", ""
+}
+
+// CheckStatusColumns checks the number of STATUS columns,
+// and returns the column names of STATUS, assuming each file would have at most two STATUS columns.
+// Parameters including:
+// e - a logger that records the error messages;
+// path - the path of the excel file;
+// j - the index of the sheets in that excel file;
+// keys - a slice that contains the header row
+func CheckStatusColumns(e *log.Logger, path string, j int, keys []string) (string, string) {
+	// create a slice that holds the column names of STATUS that matches a certain pattern
+	status := []string{}
+	for _, k := range keys {
+		matched, err := regexp.MatchString("^.*STATUS$", k) // check status's pattern
+		CheckErr(e, err)
+		if matched {
+			status = append(status, k)
+		}
+	}
+	// if len is 2, we have 2 columns of STATUS
+	if len(status) == 2 {
+		s1, s2 := status[0], status[1]
+		return s1, s2
+		// if len is 1, we have only one column of STATUS
+	} else if len(status) == 1 {
+		s1, s2 := status[0], status[0]
+		return s1, s2
+	}
+	// else would be invaid as we assume each file would have at most two STATUS columns,
+	// then an error message gets written and the program stops.
+	errlog.Invalid(e, 1, path, j)
+	os.Exit(1)
+	return "", ""
 }
