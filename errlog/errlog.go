17c16388bcb402b7fce87c04114f99ccf7e9ed3e
LynneXie1201 <lynnexie1201@gmail.com>
2016-07-12 17:02:32 -0400

    Update errlog.go

--- errlog/errlog.go
+++ errlog/errlog.go
@@ -15,13 +15,7 @@ import "log"
 // invalid - invalid value.
 func ErrorLog(e *log.Logger, path string, j int, id string, row int, t string, field string, invalid string) {
 
-	e.Println(path, "Sheet#:", j, "PTID:", id, "Row #:", row+2, "Type:", t, "Info: Invalid", field, "Value:", invalid)
-
-}
-
-// PrintErr prints the error messages of err using the logger e.
-func PrintErr(e *log.Logger, err error) {
-	e.Println(err)
+	e.Println(path, "Sheet#:", j+1, "PTID:", id, "Row #:", row+2, "Type:", t, "Info: Invalid", field, "Value:", invalid)
 }
 
 // Invalid generates different error messages using logger e.
@@ -32,11 +26,11 @@ func PrintErr(e *log.Logger, err error) {
 func Invalid(e *log.Logger, indicator int, path string, j int) {
 
 	if indicator == 0 {
-		e.Println(path, "Sheet #:", j, "INFO: This file has invalid numbers of PTID columns!")
+		e.Println(path, "Sheet #:", j+1, "INFO: This file has invalid numbers of PTID columns!")
 	} else if indicator == 1 {
-		e.Println(path, "Sheet #:", j, "INFO: This file has invalid numbers of Status columns!")
+		e.Println(path, "Sheet #:", j+1, "INFO: This file has invalid numbers of Status columns!")
 	} else if indicator == 2 {
-		e.Println(path, "Sheet #:", j, "THIS SHEET DOES NOT HAVE HEADER ROW!")
+		e.Println(path, "Sheet #:", j+1, "THIS SHEET DOES NOT HAVE HEADER ROW!")
 	}
 
 }
@@ -49,17 +43,15 @@ func Invalid(e *log.Logger, indicator int, path string, j int) {
 func Differ(e *log.Logger, indicator int, path string, j int, i int, s1 string, s2 string) {
 
 	if indicator == 0 {
-		e.Println(path, "Sheet#", j, "Row #:", i+2, "INFO: Different status values: ", s1, s2)
+		e.Println(path, "Sheet #:", j+1, "Row #:", i+2, "INFO: Different Status Values: ", s1, s2)
 	} else if indicator == 1 {
-		e.Println(path, "Sheet#", j, "Row #:", i+2, "INFO: Different PTID Values: ", s1, s2)
+		e.Println(path, "Sheet #:", j+1, "Row #:", i+2, "INFO: Different PTID Values: ", s1, s2)
 	} else if indicator == 2 {
-		e.Println(path, "Sheet#", j, "PTID:", s1, "row #", i+2, "Invalid Format of PTID!")
-	} else if indicator == 3 {
-		e.Println(path, "Sheet#", j, "Row #: ", i+2, "INFO: Invaid PTID Value:", s1)
+		e.Println(path, "Sheet #:", j+1, "Row #:", i+2, "INFO: Invaid PTID Value:", s1)
 	} else if indicator == 4 {
-		e.Println(path, "Sheet#", j, "PTID:", s1, "row #:", i+2, "INFO: iNCORRECT INFO OF REOPERATION!")
+		e.Println(path, "Sheet #:", j+1, "PTID:", s1, "Row #:", i+2, "INFO: INCORRECT INFO OF REOPERATION!")
 	} else if indicator == 5 {
-		e.Println("Path:", path, "Sheet:", j, "Row#:", i+2, "Column:", s1, s2)
+		e.Println(path, "Sheet#:", j+1, "Row#:", i+2, "Column:", s1, "INFO: Invalid Format of Date:", s2)
 	}
 
 }
